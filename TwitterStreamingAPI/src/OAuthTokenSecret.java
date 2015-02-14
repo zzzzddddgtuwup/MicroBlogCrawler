dd3f7b01276414cecd29bf7ce7d60de4b473afe4
zzzzddddgtuwup <873087751@qq.com>
2015-02-14 01:13:54 -0500

    Streaming API

---
+++ TwitterStreamingAPI/src/OAuthTokenSecret.java
@@ -0,0 +1,36 @@
+/* TweetTracker. Copyright (c) Arizona Board of Regents on behalf of Arizona State University
+ * @author shamanth
+ */
+public class OAuthTokenSecret
+{
+    String UserAccessToken;
+    String UserAccessSecret;
+
+    public String getAccessSecret() {
+        return UserAccessSecret;
+    }
+
+    public void setAccessSecret(String AccessSecret) {
+        this.UserAccessSecret = AccessSecret;
+    }
+
+    public String getAccessToken() {
+        return UserAccessToken;
+    }
+
+    public void setAccessToken(String AccessToken) {
+        this.UserAccessToken = AccessToken;
+    }
+
+    public OAuthTokenSecret(String token,String secret)
+    {
+        this.setAccessToken(token);
+        this.setAccessSecret(secret);
+    }
+
+    @Override
+    public String toString()
+    {
+       return "Access Token: "+getAccessToken()+" Access Secret: "+getAccessSecret();
+    }
+}
---
+++ TwitterStreamingAPI/src/OAuthUtils.java
@@ -0,0 +1,20 @@
+/* TweetTracker. Copyright (c) Arizona Board of Regents on behalf of Arizona State University
+ * @author shamanth
+ */
+
+public class OAuthUtils
+{
+    //Please replace the Consumer key and secret to the one representing your application.
+    public static final String CONSUMER_SECRET = "5eTq7BGpVqdr4koAtvu40dj6E0KaLgCT9tnj59ZePcD1PXTpju";
+    public static final String CONSUMER_KEY = "uQSDnl6Ke8LhRCAOHc7VwTzRG";
+    public static final String REQUEST_TOKEN_URL = "https://twitter.com/oauth/request_token";
+    public static final String AUTHORIZE_URL = "https://twitter.com/oauth/authorize";
+    public static final String ACCESS_TOKEN_URL = "https://twitter.com/oauth/access_token";
+
+    
+    //Use a JFIG file for all the configurations
+    public void ReadApplicationIdentity()
+    {
+        
+    }
+}
